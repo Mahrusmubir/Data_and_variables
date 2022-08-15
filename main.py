c8bd3f91ded742a2c809314aeb22dc507e9df5a5
Mahrusmubir <95760958+Mahrusmubir@users.noreply.github.com>
2022-08-15 13:55:08 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+character_name = "Tom"
+print(character_name + " was an IT engineer. ")
+print(character_name + " had two computers")
+print("One for work another one for personal use. ")
+print("The one " + character_name + " used for work had an i5 11400 a gtx 1660 ti with 16 gb 3200 mhz ram, ")
+print("and the one " + character_name + " used for personal use had an i5 12600k an rtx 3060 ti 16 gb ddr5 4800 mhz ram, ")
